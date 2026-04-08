49b20a4119fcbe7c359deed309c8e7063285f52e
fronre <islamhala35@gmail.com>
2026-04-08 03:10:27 +0100

    fix: updated frontend and fixed bugs

--- app/core/config.py
+++ app/core/config.py
@@ -1,10 +1,9 @@
-from pydantic import BaseSettings
+from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     DATABASE_URL: str = "sqlite:///./password_security.db"
     SECRET_KEY: str = "super_secret_for_jwt_tokens_should_be_in_env_file"
 
-    class Config:
-        env_file = ".env"
+    model_config = {"env_file": ".env"}
 
 settings = Settings()
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from app.api.routers.password_router import router as password_router
 from app.api.routers.passphrase_router import router as passphrase_router
+from app.api.routers.auth_router import router as auth_router
 from fastapi.middleware.cors import CORSMiddleware
 from app.infrastructure.database.base import Base
 from app.infrastructure.database.session import engine
@@ -26,6 +27,7 @@ app.add_middleware(
 def root():
     return {"message": "Password Security API running"}
 
-# ✅ Routers
+# Routers
 app.include_router(password_router)
 app.include_router(passphrase_router)
+app.include_router(auth_router)
